5b0de4c16102026de5194f1dbf952b6a4ccb553c
Tariq Khan <digitalgnomes@gmail.com>
2020-01-02 20:21:02 -0800

    remove console logs from app.js

--- js/app.js
+++ js/app.js
@@ -2,5 +2,3 @@ const footer = document.querySelector('footer');
 const date = new Date().toString().split(' ');
 const year = date[3];
 footer.innerHTML = '<span id="copy">&#169;</span> ' + '<span id="year">'  + year + '</span><span id="repo"><a href="https://github.com/digitalgnome/cs50w-project0">Github Project Repo</a></span>';
-console.log(`const date = ${date}`);
-console.log(`const year = date[3] = ${date[3]}`);
